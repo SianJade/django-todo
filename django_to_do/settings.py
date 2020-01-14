b698b93bfe5184449592f41fb78c853130179841
Ubuntu <ubuntu@ip-172-31-90-0.ec2.internal>
2020-01-14 11:34:52 +0000

    Fix typo in environment variable

--- django_to_do/settings.py
+++ django_to_do/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'django_to_do.wsgi.application'
 #     }
 # }
 
-DATABASES = { 'default': dj_database_url.parse(os.eviron.get('DATABASE_URL')) }
+DATABASES = { 'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
